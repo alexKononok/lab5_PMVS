52f86060beb70ac103e30d3d75b9404ce66b224e
alexKononok <alex.kononok@gmail.com>
2016-11-19 12:40:15 +0300

    Implementation calculate result

--- lab5.c
+++ lab5.c
@@ -28,6 +28,26 @@ static char first[BUF_LEN];
 static char second[BUF_LEN];
 static char operand[BUF_LEN];
 
+static struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.read = proc_read
+};
+
+static struct file_operations dev_first_fops = {
+	.owner = THIS_MODULE,
+	.write = dev_first_write,
+};
+
+static struct file_operations dev_second_fops = {
+	.owner = THIS_MODULE,
+	.write = dev_second_write
+};
+
+static struct file_operations dev_operator_fops = {
+	.owner = THIS_MODULE,
+	.write = dev_operator_write
+};
+
 static ssize_t proc_read(struct file *filp, char *buffer, size_t length, loff_t *offset) {
 	ssize_t cnt;
 	ssize_t ret;
@@ -62,5 +82,26 @@ static ssize_t proc_read(struct file *filp, char *buffer, size_t length, loff_t 
 		return 0;
 	else
 		return cnt;
+}
+
+static ssize_t dev_first_write(struct file *filp, const char *buff, size_t len, loff_t *off) {
+	if (copy_from_user(first, buff, len)) {
+		return -EFAULT;
+	}
+	return len;
+}
+
+static ssize_t dev_second_write(struct file *filp, const char *buff, size_t len, loff_t *off) {
+	if (copy_from_user(second, buff, len)) {
+		return -EFAULT;
+	}
+	return len;
+}
+
+static ssize_t dev_operator_write(struct file *filp, const char *buff, size_t len, loff_t *off) {
+	if (copy_from_user(operand, buff, len)) {
+		return -EFAULT;
+	}
+	return len;
 }
 
